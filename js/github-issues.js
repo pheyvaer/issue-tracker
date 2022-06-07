303059c1de79e88063e17256a2a9deb90c09332f
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2022-06-07 09:44:02 +0200

    Support multiple assignees.

--- js/github-issues.js
+++ js/github-issues.js
@@ -20,12 +20,12 @@ export async function convertIssuesToGridRecords(issues, solidFetch, storageLoca
       const {projects, dueDate, milestones} = await getAnnotationsForIssue(issue.url, solidFetch, storageLocationUrl);
       const record = {
         title: issue.title,
-        assignee: issue.assignee ? issue.assignee.login : null,
+        assignee: _getAssignees(issue),
         number: issue.number,
-        state: getState(issue),
-        type: getTypeOfIssue(issue),
+        state: _getState(issue),
+        type: _getTypeOfIssue(issue),
         url: issue.url,
-        labels: removeRedundantLabels(issue.labels),
+        labels: _removeRedundantLabels(issue.labels),
         creator: issue.user.login,
         githubUrl: issue.html_url,
         projects,
@@ -40,7 +40,7 @@ export async function convertIssuesToGridRecords(issues, solidFetch, storageLoca
   return records;
 }
 
-function getTypeOfIssue(issue) {
+function _getTypeOfIssue(issue) {
   const labels = issue.labels;
 
   let i = 0;
@@ -54,21 +54,34 @@ function getTypeOfIssue(issue) {
   }
 }
 
-function removeRedundantLabels(labels) {
+function _removeRedundantLabels(labels) {
   labels = labels.map(label => label.name);
   return labels.filter(label => label !== 'challenge' && label !== 'scenario' && label !== 'completed');
 }
 
-function isCompleted(issue) {
+function _isCompleted(issue) {
   const labels = issue.labels.map(label => label.name);
   return labels.includes('completed');
 }
 
-function getState(issue) {
-  if (isCompleted(issue)) {
+function _getState(issue) {
+  if (_isCompleted(issue)) {
     return 'Completed';
   } else {
     const {state} = issue;
     return state[0].toUpperCase() + state.substring(1);
   }
 }
+
+function _getAssignees(issue) {
+  const {assignee, assignees} = issue;
+
+  if (assignees.length > 0) {
+    return assignees.map(a => a.login);
+  } else if (assignee) {
+    return assignee.login;
+  }
+
+  return null;
+}
+
